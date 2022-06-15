8edd2a59f8e7c3f6438c305b25f02c968e78df32
shazam <nicholas.s.cysne@gmail.com>
2022-06-15 20:29:20 -0300

    Uning namespace

--- main.cpp
+++ main.cpp
@@ -2,39 +2,41 @@
 #include "line.h"
 #include "point.h"
 
+using namespace std;
+
 int main() {
     double x, y;
     Point p1(0, 0), p2(0, 0);
     Line l1(p1, p2), l2(p1, p2);
 
-    std::cout << "---------------------------------------------" << std::endl;
-    std::cout << "Program to find the intersection of two lines" << std::endl;
-    std::cout << "---------------------------------------------" << std::endl;
+    cout << "---------------------------------------------" << endl;
+    cout << "Program to find the intersection of two lines" << endl;
+    cout << "---------------------------------------------" << endl;
 
-    std::cout << "Type in two points of the first line" << std::endl;
-    std::cin >> x >> y;
+    cout << "Type in two points of the first line" << endl;
+    cin >> x >> y;
     p1 = Point(x, y);
-    std::cin >> x >> y;
+    cin >> x >> y;
     p2 = Point(x, y);
     l1 = Line(p1, p2);
 
-    std::cout << "Type in two points of the second line << std::endl;
-    std::cin >> x >> y;
+    cout << "Type in two points of the second line << endl;
+    cin >> x >> y;
     p1 = Point(x, y);
-    std::cin >> x >> y;
+    cin >> x >> y;
     p2 = Point(x, y);
     l2 = Line(p1, p2);
 
     if (l1.parallelWith(l2)) {
         if (l1.isEqualTo(l2))
-            std::cout << "The intersection is the line you gave\n";
+            cout << "The intersection is the line you gave\n";
         else
-            std::cout << "There is no intersection\n";
+            cout << "There is no intersection\n";
     } else {
-        std::cout << "The intersection is a point\n";
-        std::cout << "And its coordinates are: ";
+        cout << "The intersection is a point\n";
+        cout << "And its coordinates are: ";
         p1 = l1.intersectWith(l2);
-        std::cout << p1.getX() << p1.getY() << "\n";
+        cout << p1.getX() << p1.getY() << "\n";
     }
 
     return 0;
